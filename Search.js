b9111e1289ee2a1ce88517f580d0f247b7e72a79
chalalala <neko_hime1212@yahoo.com>
2020-10-13 13:14:29 +0700

    Update search function

--- Search.js
+++ Search.js
@@ -1,97 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { ActivityIndicator, StyleSheet, Text, View, FlatList, Linking, Image, TextInput } from 'react-native';
 import moment from 'moment';
 import { Card, Button } from 'react-native-elements';
 import { Icon } from 'react-native-elements';
 import filterForUniqueItems from './filterForUniqueItems.js';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const key = "84af92544a4249b0a1b41e3c823177c9";
+const key = "0e639301ff494ab491315d63255a7d05";
+const Stack = createStackNavigator();
 
-export default function News(){
-    const [loading, setLoading] = useState(false);
+export default class Search extends Component{
+  render(){
+    return(
+      <Stack.Navigator 
+        initialRouteName="Search"
+      >
+        <Stack.Screen name="Search" component={SearchContainer}/>
+        <Stack.Screen name="Results" component={Results}/>
+      </Stack.Navigator>
+    )
+  }
+}
+
+const SearchContainer = ({navigation}) => {
     const [query, setQuery] = useState([]);
-    const [articles, setArticles] = useState([]);
-    const [hasErrored, setHasApiError] = useState(false);
-    const [lastPageReached, setLastPageReached] = useState(false);
-    
-    const getNews = async () => {
-        setArticles([]);
-        setLoading(true);
-        try{
-            const response = await fetch(`https://newsapi.org/v2/everything?qInTitle=${query}&apiKey=${key}`);
-            const jsonData = await response.json();
-            const hasMoreArticles = jsonData.articles.length > 0;
-            if (hasMoreArticles){
-                setArticles(jsonData.articles);
-            }
-            else{
-                setLastPageReached(false);
-            }
-        }
-        catch(error){
-            setHasApiError(true);
-      };
-  
-      setLoading(false);
-    };
-  
-    useEffect(() => {getNews()},[]);
-  
-    if (loading){
-      return(
-        <View style={styles.container}>
-          <ActivityIndicator size="large" loading={loading} color="#0000ff"/>
-        </View>
-      )
-    }
-    
-    const renderArticleItem = ({item}) => {
-      return(
-        <Card>
-          <View style={styles.row}>
-            <Text style={styles.title}>{item.title}</Text>
-          </View>
-          <Image 
-            source={{uri: item.urlToImage}}
-            style={styles.image}
-          />
-          <View style={styles.row}>
-            <Text style={styles.label}>Source: </Text>
-            <Text style={styles.info}>{item.source.name}</Text>
-          </View>
-          <Text style={{marginBottom:10}}>{item.content}</Text>
-          <View style={styles.row}>
-            <Text style={styles.row}>
-              <Text style={styles.label}>Published: </Text>
-              <Text style={styles.info}>
-                {moment(item.publishedAt).format('LLL')}
-              </Text>
-            </Text>
-          </View>
-          <Button icon={<Icon/>} title="Read more" backgroundColor="#03A9F4" onPress={() => onPress(item.url)}/>
-        </Card>
-      );
-    }; 
-  
-    const onPress = url => {
-      Linking.canOpenURL(url).then(supported => {
-        if (supported){
-          Linking.openURL(url);
-        }
-        else{
-          console.log(`Don't know how to open URL: ${url}`);
-        }
-      })
-    }
-    // if (hasErrored){
-    //     return(
-    //       <View style={styles.container}>
-    //         <Text>Error =(</Text>
-    //       </View>
-    //     )
-    // }
-    return (
+
+    return(
       <View style={styles.container}>
         <View style={styles.row}>
           <Text style={styles.title}>Search for articles</Text>
@@ -101,23 +36,113 @@ export default function News(){
             style={styles.queryInput}
             placeholder="Enter keywords"
         />
-        <TouchableOpacity style={styles.searchButton} onClick={getNews}>
-            <Text style={styles.textButton}>Search</Text>
+        <TouchableOpacity style={styles.searchButton} 
+          onPress={()=>navigation.navigate("Results", query)}
+        >
+          <Text style={styles.textButton}>Search</Text>
         </TouchableOpacity>
-        <Text>Number of results: {articles.length}</Text>    
-        <FlatList
-          data={articles}
-          onEndReached={getNews}
-          onEndReachedThreshold={1}
-          renderItem={renderArticleItem}
-          keyExtractor={item => item.title}
-          ListFooterComponent={lastPageReached ?
-            <Text style={styles.info}>No more articles</Text> : 
-            <ActivityIndicator size="large" loading={loading}/>
-          }
-        />
       </View>
+    )
+}
+
+const Results = ({route}) => {
+  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
+  const [hasErrored, setHasApiError] = useState(false);
+  const [lastPageReached, setLastPageReached] = useState(false);
+  
+  const getNews = async () => {
+      setLoading(true);
+      try{
+          const response = await fetch(`https://newsapi.org/v2/everything?qInTitle=${route.params}&apiKey=${key}`);
+          const jsonData = await response.json();
+          const hasMoreArticles = jsonData.articles.length > 0;
+          if (hasMoreArticles){
+              setArticles(jsonData.articles);
+          }
+          else{
+              setLastPageReached(false);
+          }
+      }
+      catch(error){
+          setHasApiError(true);
+    };
+
+    setLoading(false);
+  };
+
+  useEffect(() => {getNews()},[]);
+
+  if (loading){
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator size="large" loading={loading} color="#0000ff"/>
+      </View>
+    )
+  }
+  
+  const renderArticleItem = ({item}) => {
+    return(
+      <Card>
+        <View style={styles.row}>
+          <Text style={styles.title}>{item.title}</Text>
+        </View>
+        <Image 
+          source={{uri: item.urlToImage}}
+          style={styles.image}
+        />
+        <View style={styles.row}>
+          <Text style={styles.label}>Source: </Text>
+          <Text style={styles.info}>{item.source.name}</Text>
+        </View>
+        <Text style={{marginBottom:10}}>{item.content}</Text>
+        <View style={styles.row}>
+          <Text style={styles.row}>
+            <Text style={styles.label}>Published: </Text>
+            <Text style={styles.info}>
+              {moment(item.publishedAt).format('LLL')}
+            </Text>
+          </Text>
+        </View>
+        <Button icon={<Icon/>} title="Read more" backgroundColor="#03A9F4" onPress={() => onPress(item.url)}/>
+      </Card>
     );
+  }; 
+
+  const onPress = url => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported){
+        Linking.openURL(url);
+      }
+      else{
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    })
+  }
+  if (hasErrored){
+      return(
+        <View style={styles.container}>
+          <Text>Error =(</Text>
+        </View>
+      )
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Number of results: {articles.length}</Text>
+      <Text>Query: {route.params}</Text>    
+      <FlatList
+        data={articles}
+        onEndReached={getNews}
+        onEndReachedThreshold={1}
+        renderItem={renderArticleItem}
+        keyExtractor={item => item.title}
+        ListFooterComponent={lastPageReached ?
+          <Text style={styles.info}>No more articles</Text> : 
+          <ActivityIndicator size="large" loading={loading}/>
+        }
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -128,10 +153,8 @@ const styles = StyleSheet.create({
     },
     container: {
       flex: 1,
-      marginTop: 40,
       alignItems: 'center',
       backgroundColor: '#fff',
-      justifyContent: 'center'
     },
     header: {
       height: 30,
